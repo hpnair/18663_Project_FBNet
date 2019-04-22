e41aaa6caad424d39c399f0befc6dd3ec4074c5a
SaiVineethKalluruSrinivas <skalluru@andrew.cmu.edu>
2019-04-22 18:34:38 -0400

    Removed condense net folder

--- CondenseNet/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .densenet import DenseNet as densenet
-from .condensenet import CondenseNet as condensenet
-from .condensenet_converted import CondenseNet as condensenet_converted
-from .densenet_LGC import DenseNet_LGC as densenet_LGC
