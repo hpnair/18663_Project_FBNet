1ff4425fe4185eb3a199029784730a5d0f36c6a5
Harideep Nair <hpnair@br018.pvt.bridges.psc.edu>
2019-05-10 22:06:26 -0400

    Cleanup

--- hanna_pytorch/train_cifar10.py
+++ hanna_pytorch/train_cifar10.py
@@ -9,9 +9,9 @@ import argparse
 import time
 import os
 
-from model import Trainer, FBNet
+from supernet import Trainer, FBNet
 from data import get_ds
-from blocks import get_blocks
+from candblks import get_blocks
 from utils import _logger, _set_file
 
 
